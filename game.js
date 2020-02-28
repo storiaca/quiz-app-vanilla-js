4c123d11c8247024653aaf28be978cf4f00b5cb9
Mac <storiaca@gmail.com>
2020-02-28 10:39:41 +0100

    Create Head's Up Display(HUD)

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -56,6 +58,8 @@ getNewQuestion = () => {
     return window.location.assign("/end.html");
   }
   questionCounter++;
+  questionCounterText.innerText = `${questionCounter} / ${MAX_QUESTIONS}`;
+
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -80,6 +84,10 @@ choices.forEach(choice => {
     if (selectedAnswer == currentQuestion.answer) {
       classToApply = "correct";
     }
+
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
     //console.log();
     selectedChoice.parentElement.classList.add(classToApply);
     setTimeout(() => {
@@ -88,5 +96,10 @@ choices.forEach(choice => {
     }, 1000);
   });
 });
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
 
 startGame();
