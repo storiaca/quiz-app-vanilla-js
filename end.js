31872d0df381141532f83e92d0ec0de1e83be295
storiaca <storiaca@gmail.com>
2020-03-20 19:28:21 +0100

    Save high scores in local storage finish

--- end.js
+++ end.js
@@ -4,9 +4,8 @@ const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 const highScores = JSON.parse(localStorage.getItem("highScores"));
-console.log(highScores);
 
-console.log(JSON.parse(localStorage.getItem("highScores")));
+const MAX_HIGH_SCORE = 5;
 
 finalScore.innerText = mostRecentScore;
 
@@ -15,14 +14,19 @@ username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
 });
 saveHighScore = e => {
-  console.log("clicked the save button");
   e.preventDefault();
 
   const score = {
-    score: mostRecentScore,
+    score: Math.floor(Math.random() * 100),
     name: username.value
   };
 
   highScores.push(score);
-  console.log(highScoress);
+  console.log(highScores);
+
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("/");
 };
